8761a3930a6d561a2d841591363d7b75841fc687
Jean Carlos COYLA <jeancarlos.coyla@bnpparibas.com>
2019-09-23 18:54:18 +0200

    chore: add fake data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,40 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Users;
+use App\Entity\Questions;
+use App\Entity\Answers;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-
+use Faker;
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
         // $manager->persist($product);
+        $faker = Faker\Factory::create('fr_FR');
+        //users 
+        // on créé 10 personnes
+        for ($i = 0; $i < 10; $i++) {
+            $user = new Users();
+            $user->setName($faker->name);
+            $manager->persist($user);
 
+            $question = new Questions();
+            $question->setTitle($faker->title);
+            $question->setContent($faker->realText);
+            $question->setUser($user);
+            $manager->persist($question);
+
+            $answer = new Answers();
+            $answer->setContent($faker->realText);
+            $answer->setStatus($faker->boolean);
+            $answer->setQuestion($question);
+            $manager->persist($answer);
+        }
+
+        //answers
         $manager->flush();
     }
 }
